74961f88b558125769b340c6a85844258804e636
Kim Do-Hee <rlaehgml03@gmail.com>
2021-04-14 16:53:55 +0900

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -1,21 +1,19 @@
 var dates = document.getElementById('date');
 var clock = document.getElementById('clock');
-var today = new Date();
 
 var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
-var year = today.getFullYear();
-var month = today.getMonth() + 1;
-var date = today.getDate();
-var day = today.getDay();
-var d = days[day];
-var m = months[month];
-dates.innerHTML = `${date} ${m} ${year} &nbsp;${d}`;
-
-
 function getTime() {
-   
+   var today = new Date();
+   var year = today.getFullYear();
+   var month = today.getMonth() + 1;
+   var date = today.getDate();
+   var day = today.getDay();
+   var d = days[day];
+   var m = months[month];
+   dates.innerHTML = `${date} ${m} ${year} &nbsp;${d}`;
+
    var hour = today.getHours();
    var minute = today.getMinutes();
    var second = today.getSeconds();
